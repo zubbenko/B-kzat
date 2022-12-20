b8e13c9018d645f4bc7578ba6d0512c2e293017a
zubbenko <119858294+zubbenko@users.noreply.github.com>
2022-12-21 01:01:22 +0600

    Create init.py

---
+++ init.py
@@ -0,0 +1,23 @@
+
+from hw5 import Hero
+from hw5 import Hero_super
+
+from turtle import *
+
+color('green')
+
+bgcolor('black')
+
+speed(200)
+
+hideturtle()
+
+b = 0
+
+while b < 200:
+
+  right( b)
+
+  forward(b * 3)
+
+  b = b + 1
